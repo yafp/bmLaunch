6f2175c114c99fa94bca1ca5548de0e49985f894
yafp <fidel@yafp.de>
2015-10-13 17:29:13 +0200

    20151013 - Implementing #6 - Search & highlight on index.html

---
+++ data/js/bmLaunch.js
@@ -0,0 +1,15 @@
+// source for Highlight lib
+//
+// http://bartaz.github.io/sandbox.js/jquery.highlight.html
+
+function doSearch()
+{
+   currentSearch = $("#searchBox").val();
+   console.log("Current search phrase: "+currentSearch);
+
+   // remove all existing highlights
+   $("#bookmarkDiv").unhighlight();
+
+   // highlight everything in the div 'bookmarkDiv' whicbh contains this string
+   $("#bookmarkDiv").highlight(currentSearch, { caseSensitive: false });
+}
