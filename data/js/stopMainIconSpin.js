d9e14ebfbee72bd2a18d0e79a8a063bdbb01a9bf
yafp <fidel@yafp.de>
2015-10-15 21:18:22 +0200

    20151015 - Added new file to stop the mainicon animation - which is started on page load -> #9

---
+++ data/js/stopMainIconSpin.js
@@ -0,0 +1,7 @@
+console.log("/data/js/stopMainIconSpin.js: Stopping MainIcon animation now");
+//alert("Stopping spinning mainicon now");
+document.getElementById("mainIcon").className = "fa fa-external-link-square";
+console.log("/data/js/stopMainIconSpin.js: Stopped MainIcon animation");
+
+
+//$("#bookmarkDiv").fadeIn(4000); // hide bookmarks
